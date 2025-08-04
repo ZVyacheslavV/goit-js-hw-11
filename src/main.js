da187f64e2d4b874b8ed03d9f53d727008948d2e
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-08-04 18:46:54 +0300

    init hw, test main js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,48 @@
-fetch('https://jsonplaceholder.typicode.com/users')
+/* const fetchUsersBtn = document.querySelector('.btn');
+const fetchUsersBtnTwo = document.querySelector('.btn-two');
+
+fetchUsersBtn.addEventListener('click', () => {
+  fetch('https://jsonplaceholder.typicode.com/users?_limit=7&_sort=name')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(users => {
+      // Дані від бекенда
+      console.log('limit-sort:', users);
+    })
+    .catch(error => console.log(error));
+});
+
+fetchUsersBtnTwo.addEventListener('click', () => {
+  fetch('https://jsonplaceholder.typicode.com/users?_sort=name&_limit=7')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(users => {
+      // Дані від бекенда
+      console.log('sort-limit:', users);
+    })
+    .catch(error => console.log(error));
+});
+ */
+
+import axios from 'axios';
+
+axios
+  .get('https://jsonplaceholder.typicode.com/users')
   .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    // Data handling
-    console.log('Отримані дані:', data);
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
   })
   .catch(error => {
-    // Error handling
-    console.log('Помилка запиту:', error);
+    console.log(error);
   });
